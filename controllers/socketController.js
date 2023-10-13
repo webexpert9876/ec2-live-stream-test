ba14b6fb748f07f0af5fade83794d8c67ad726c5
webexpert9876 <webexpert9876@gmail.com>
2023-10-13 16:11:10 +0530

    Worked on chatting

--- controllers/socketController.js
+++ controllers/socketController.js
@@ -6,7 +6,6 @@ const ErrorHandler = require('../utils/errorHandler');
 var userLists = {};
 var channelViewerLists = {};
 var liveViewerCounts = {};
-var liveViewerCount = 0;
 const { v4: uuidv4 } = require('uuid');
 
 exports.handleRoomJoining = (socket)=>{
@@ -39,9 +38,6 @@ exports.handleRoomJoining = (socket)=>{
 
 exports.handleVideoViewerJoining = (socket)=>{
   socket.on('joinLiveViewerRoom', async (roomId, {streamId}) => {
-    console.log('connected', roomId)
-    console.log('streamId', streamId)
-    console.log('channelViewerLists', channelViewerLists)
     socket.join(roomId);
     socket.roomId = roomId;
     socket.liveStreamId = streamId;
@@ -68,53 +64,40 @@ exports.handleVideoViewerJoining = (socket)=>{
       }
 
     } else {
-      console.log('connected else', roomId)
       // channelViewerLists[roomId] = {viewer: 1}
       channelViewerLists[roomId] = []
       liveViewerCounts[roomId] = {liveViewer: 1}
       channelViewerLists[roomId].push(socket.userId);
       // liveViewerCount += 1;
     }
-    console.log(channelViewerLists);
     // console.log(`User ${socket.id} joined room: ${roomId}`);
-    console.log(`User ${socket.userId} joined room: ${roomId}`);
-    // liveStreamingModel.
   });
 };
 
 exports.handleStreamChat = (socket)=>{
   socket.on('sendMessage', async ({ roomId, message, userId, userName }) => {
-    console.log('---------------------------------------------- ')
-    console.log('socket.userId ', userId)
-    console.log('userLists old ', userLists)
     socket.originalUserId = userId;
-
     if(socket.originalUserId){
-      console.log('created userid ')
       // socket.userId = userId;
-      
       if(userLists.hasOwnProperty(roomId)){
         if(!userLists[roomId].includes(userId)){
           userLists[roomId].push(userId);
           // let viewerCount = userLists[roomId].length;
           // socket.to(roomId).emit('viewerCounts', { viewerCount });
-          console.log('if', userLists)
-          // const chatInfo = await chatMessageModel.create({userId, message, videoId: roomId});
-          // console.log('if chatInfo', chatInfo)
+          const chatInfo = await chatMessageModel.create({userId, message, videoId: roomId});
+          socket.to(roomId).emit('receiveMessage', { roomId, message, sender: userName });
+        } else {
+          
+          const chatInfo = await chatMessageModel.create({userId, message, videoId: roomId});
           socket.to(roomId).emit('receiveMessage', { roomId, message, sender: userName });
         }
       } else {
         userLists[roomId] = []
         userLists[roomId].push(userId);
-        console.log('else', userLists)
-        // const chatInfo = await chatMessageModel.create({userId, message, videoId: roomId});
-        // console.log('else chatInfo', chatInfo)
+        const chatInfo = await chatMessageModel.create({userId, message, videoId: roomId});
         socket.to(roomId).emit('receiveMessage', { roomId, message, sender: userName });
       }
     }
-
-    console.log('userLists new ', userLists)
-    console.log('---------------------------------------------- ')
   });
 };
 
@@ -148,10 +131,8 @@ exports.handleSocketDisconnect = (socket)=>{
     userId = socket.userId;
     roomId = socket.roomId;
     streamId = socket.liveStreamId;
-    console.log('User userid', userId);
-    console.log('User roomid', roomId);
-    console.log('User streamId', streamId);
-    console.log('---------------------------------------------');
+
+    // console.log('---------------------------------------------');
     // if(userLists.hasOwnProperty(roomId)){
     //   if(userLists[roomId].includes(userId)){
     //     let indexToRemove = userLists[roomId].indexOf(userId)
@@ -169,11 +150,9 @@ exports.handleSocketDisconnect = (socket)=>{
     //   }
     // }
 
+    // console.log(' before channelViewerLists', channelViewerLists);
     if(channelViewerLists.hasOwnProperty(roomId)){
-      console.log('in ============>')
-      console.log('in ============>', channelViewerLists[roomId].includes(userId))
       if(channelViewerLists[roomId].includes(userId)){
-        console.log('user confirm ============>')
         let indexToRemove = channelViewerLists[roomId].indexOf(userId)
         if (indexToRemove >= 0 && indexToRemove < channelViewerLists[roomId].length) {
           channelViewerLists[roomId].splice(indexToRemove, 1);
@@ -187,14 +166,35 @@ exports.handleSocketDisconnect = (socket)=>{
               viewers: liveViewerCounts[roomId].liveViewer
             });
           }
-
+          // console.log(' after channelViewerLists', channelViewerLists);
           socket.to(roomId).emit('viewerCounts', { viewerCount: liveViewerCounts[roomId].liveViewer });
+          // socket.leave(roomId);
+          // console.log('Element at index', indexToRemove, 'removed from the array.');
+          // console.log('userList', userLists);
+        } else {
+          console.log('Invalid index provided.');
+          console.log('channelViewerLists', channelViewerLists);
+        }
+      }
+    }
+
+
+// console.log(' before userLists', userLists);
+    if(userLists.hasOwnProperty(roomId)){
+
+      if(userLists[roomId].includes(socket.originalUserId)){
+
+        let indexToRemove = userLists[roomId].indexOf(socket.originalUserId)
+        if (indexToRemove >= 0 && indexToRemove < userLists[roomId].length) {
+          userLists[roomId].splice(indexToRemove, 1);
+
+          // console.log(' after userLists', userLists);
           socket.leave(roomId);
           // console.log('Element at index', indexToRemove, 'removed from the array.');
           // console.log('userList', userLists);
         } else {
           console.log('Invalid index provided.');
-          console.log('userList', channelViewerLists);
+          console.log('userList', userLists);
         }
       }
     }
