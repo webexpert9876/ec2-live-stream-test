ac4b35f9ccc0347779d69a7602678cfb0776ff08
webexpert9876 <webexpert9876@gmail.com>
2023-11-03 17:30:57 +0530

    testing ip

--- controllers/videoViewController.js
+++ controllers/videoViewController.js
@@ -1,26 +1,90 @@
 const videoViewModel= require('../models/videoViewModel');
 const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
 const ErrorHandler = require('../utils/errorHandler');
+const videoModel = require('../models/videoModel');
 
 // Create video view information
 exports.createVideoView = catchAsyncErrors( async (req, res)=>{
-    // console.log('ip ', req)
-    console.log('ip ', req.ip)
-    console.log('req.clientIp;', req.clientIp)
-    console.log('req.connection.remoteAddress;', req.connection.remoteAddress)
-    console.log('req.socket.remoteAddress;', req.socket.remoteAddress)
-    const ip =  req.ip;
     const clientIp =  req.clientIp;
-    var sip = req.headers['x-forwarded-for'] || req.socket.remoteAddress 
-    const conIp = req.connection.remoteAddress
-    // const viewData = await videoViewModel.create(req.body);
+    let viewCreateData={};
+    let userFound;
+    let ipFound;
+    let userId;
+    let viewData;
+    
+    const videoInfo = await videoModel.findById({_id: req.body.videoId});
+    
+    let videoId = req.body.videoId;
+
+    if(videoInfo){
+    
+        if(req.body.userId){
+            userId = req.body.userId;
+            
+            viewCreateData = {
+                userId: userId,
+                videoId: videoId,
+                userIpAddress: clientIp
+            }
+    
+            userFound = await videoViewModel.findOne({$and: [
+                {userId: userId},
+                {videoId: videoId}
+            ]});
+
+            console.log(userFound);
+
+            if(!userFound){
+
+                ipFound = await videoViewModel.findOne({$and: [
+                    {userIpAddress: clientIp},
+                    {videoId: videoId}
+                ]});
+
+                if(ipFound){
+
+                    if(userId != ipFound.userId){
+                        viewData = await videoViewModel.create(viewCreateData);
+                    } else {
+                        viewData = await videoViewModel.findByIdAndUpdate(ipFound._id, {
+                            userId: userId
+                        });
+                    }
+                } else {
+                    viewData = await videoViewModel.create(viewCreateData);
+                }
+                
+                if(viewData){
+                    videoInfo.views = videoInfo.views + 1;
+                    videoInfo.save({ validateBeforeSave: false });
+                }
+            }
+    
+        } else {
+            viewCreateData = {
+                videoId: videoId,
+                userIpAddress: clientIp
+            }
+
+            ipFound = await videoViewModel.findOne({$and: [
+                {userIpAddress: clientIp},
+                {videoId: videoId}
+            ]});
+
+            if(!ipFound){
+
+                viewData = await videoViewModel.create(viewCreateData);
+                
+                if(viewData){
+                    videoInfo.views = videoInfo.views + 1;
+                    videoInfo.save({ validateBeforeSave: false });
+                }
+            }
+        }
+    }
 
     res.status(200).json({
         success: true,
-        ip,
-        clientIp,
-        conIp,
-        sip
-        // viewData: viewData
+        viewData
     });
 });
