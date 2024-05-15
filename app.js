b51668c6254b33795a77fcd191f039fbb7d8ee17
webexpert9876 <webexpert9876@gmail.com>
2024-05-15 13:25:51 +0530

    changed backend google callback url

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ app.use(passport.session());
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:8080/prod/auth/google/callback"
+    callbackURL: process.env.BACKEND_CALLBACK_URL
   },
   googleSignInValidation
 ));
